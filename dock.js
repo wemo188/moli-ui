b323ee2187136c2c65add820bc4fe8ca6f15f023
wemo188 <3666470636@qq.com>
2026-04-15 01:59:10 +0800

    更新 dock.js

--- dock.js
+++ dock.js
@@ -4,22 +4,18 @@
   if (!App) return;
 
   var Dock = {
-
     init: function() {
-      App.safeOn('#dockMine', 'click', function() {
-        App.showToast('我的 - 开发中');
-      });
-
+      // dockMine 由 user.js (Social) 绑定
       App.safeOn('#dockLong', 'click', function() {
-        App.showToast('长剧情 - 开发中');
+        App.showToast('长剧情 · 开发中');
       });
 
       App.safeOn('#dockShort', 'click', function() {
-        App.showToast('短对话 - 开发中');
+        App.showToast('短对话 · 开发中');
       });
 
       App.safeOn('#dockCheck', 'click', function() {
-        App.showToast('查岗 - 开发中');
+        App.showToast('角色 · 开发中');
       });
     }
   };
