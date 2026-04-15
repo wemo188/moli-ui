60b71b8bc0a4900e16edd7403ce5c3146e0a7d1d
wemo188 <3666470636@qq.com>
2026-04-15 22:27:01 +0800

    更新 dock.js

--- dock.js
+++ dock.js
@@ -6,11 +6,19 @@
   var Dock = {
     init: function() {
       App.safeOn('#dockMine', 'click', function() {
-        if (App.user) App.user.open();
+        if (App.user && App.user.open) {
+          App.user.open();
+        } else {
+          App.showToast('用户模块加载中...');
+        }
       });
 
       App.safeOn('#dockLong', 'click', function() {
-        if (App.character) App.character.open();
+        if (App.character && App.character.open) {
+          App.character.open();
+        } else {
+          App.showToast('角色模块加载中...');
+        }
       });
 
       App.safeOn('#dockShort', 'click', function() {
