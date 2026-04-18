86dff0c1bd4950e5a93390ae09090edd1b974885
wemo188 <3666470636@qq.com>
2026-04-18 23:40:11 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -1470,22 +1470,16 @@ App.init = function() {
     App.initMainPages();
   };
 
-    window.addEventListener('load', function() {
+      window.addEventListener('load', function() {
     App.init();
 
-    // 注册 Service Worker
+    // 清除旧的 Service Worker
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./sw.js').then(function(reg) {
-        // 检测到新版本时自动更新
-        reg.addEventListener('updatefound', function() {
-          var newWorker = reg.installing;
-          newWorker.addEventListener('statechange', function() {
-            if (newWorker.state === 'activated') {
-              App.showToast('已更新到最新版本');
-            }
-          });
-        });
+      navigator.serviceWorker.getRegistrations().then(function(regs) {
+        regs.forEach(function(reg) { reg.unregister(); });
+      });
+      caches.keys().then(function(names) {
+        names.forEach(function(name) { caches.delete(name); });
       });
     }
   });
-})();
