489d24af8090e9364fc5cc106af6ed54ab7b4ab3
wemo188 <3666470636@qq.com>
2026-04-20 21:37:48 +0800

    更新 user.js

--- user.js
+++ user.js
@@ -135,7 +135,6 @@
               '</div>' +
               '<div class="p14-screen-wrap"><div class="p14-screen">' +
                 '<div class="p14-screen-badge">' + (isActive ? '<div class="p14-badge-dot"></div><div class="p14-badge-text">ACTIVE</div>' : '') + '</div>' +
-                '<div class="p14-screen-no">NO.' + idx + '</div>' +
                 '<div class="p14-screen-content">' +
                   '<div class="p14-avatar-wrap" data-uid="' + u.id + '"><div class="p14-avatar">' + avatarHtml + '</div><div class="p14-avatar-ov"><svg viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg></div></div>' +
                   '<div class="p14-info"><div class="p14-name">' + App.esc(u.realName || '未命名') + '</div>' +
